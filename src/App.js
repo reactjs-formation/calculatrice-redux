140b97f429a58d3f9ac3f58283cdf7c8485e5653
codeurh24 <cci.corlouer@gmail.com>
2020-06-05 13:21:23 +0200

    change style + pre action redux

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ import './App.css';
 
 const mapStateToProps = state => ({state: state})
 
-const mapDispatchToProps = dispatch => ({ dispatch: arg => dispatch(arg) });
+const mapDispatchToProps = dispatch => ({ 
+  dispatch: arg => dispatch(arg) ,
+  screen: number => dispatch({type: types.SCREEN, number:number}),
+  addOp: () => dispatch({type: types.ADD}),
+  subOp: () => dispatch({type: types.SUB}),
+  divOp: () => dispatch({type: types.DIV}),
+  mulOp: () => dispatch({type: types.MUL}),
+  equOp: () => dispatch({type: types.EQU}),
+});
 
 function App(props) {
 
@@ -21,28 +29,28 @@ function App(props) {
             <div className="bar"></div>
             <div className="screen">{props.state.screen}</div>
             <div className="pad">
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:7})}>7</button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:8})}>8</button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:9})}>9</button>
-              <button onClick={() => props.dispatch({type: types.ADD})}>
+              <button onClick={() => props.screen(7)}>7</button>
+              <button onClick={() => props.screen(8)}>8</button>
+              <button onClick={() => props.screen(9)}>9</button>
+              <button onClick={props.addOp}>
                 <i className="fas fa-plus"></i>
               </button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:4})}>4</button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:5})}>5</button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:6})}>6</button>
-              <button onClick={() => props.dispatch({type: types.SUB})}>
+              <button onClick={() => props.screen(4)}>4</button>
+              <button onClick={() => props.screen(5)}>5</button>
+              <button onClick={() => props.screen(6)}>6</button>
+              <button onClick={props.subOp}>
                 <i className="fas fa-minus"></i>
               </button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:1})}>1</button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:2})}>2</button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:3})}>3</button>
-              <button onClick={() => props.dispatch({type: types.DIV})}>
+              <button onClick={() => props.screen(1)}>1</button>
+              <button onClick={() => props.screen(2)}>2</button>
+              <button onClick={() => props.screen(3)}>3</button>
+              <button onClick={props.divOp}>
                 <i className="fas fa-divide"></i>
               </button>
-              <button onClick={() => props.dispatch({type: types.SCREEN, number:0})}>0</button>
+              <button onClick={() => props.screen(0)}>0</button>
               <button onClick={() => props.dispatch({type: types.RESET})}>C</button>
-              <button onClick={() => props.dispatch({type: types.EQU})}>=</button>
-              <button onClick={() => props.dispatch({type: types.MUL})}>
+              <button onClick={props.equOp}>=</button>
+              <button onClick={props.mulOp}>
                 <i className="fas fa-times"></i>
               </button>
             </div>
